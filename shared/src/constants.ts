e665bde0c83e42f67c7288e73cb6c131a45b7cdd
LimeWub <myrto.kontouli@gmail.com>
2026-02-21 09:54:56 +0000

    feat: server TICK_RATE up to 60

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -1,7 +1,7 @@
 export const MAX_PLAYERS = 12;
 export const MIN_PLAYERS = 2;
 export const DEFAULT_LIVES = 3;
-export const TICK_RATE = 30;
+export const TICK_RATE = 60;
 export const ARENA_RADIUS = 350;
 export const BALL_RADIUS = 8;
 export const BALL_SPEED = 5;
