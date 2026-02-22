ba6ccc66c6c2a8a75e52e242f491c49cd34b8723
LimeWub <myrto.kontouli@gmail.com>
2026-02-22 08:46:47 +0000

    remove shared dist tracking (?)

--- shared/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./types.js";
-export * from "./constants.js";
-export * from "./geometry.js";
-export * from "./physics.js";
-export * from "./goals-physics.js";
-//# sourceMappingURL=index.d.ts.map
